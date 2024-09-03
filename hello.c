bd14813d7932c8cb33a44a88383160d30b20b5b4
zandman20 <alexzanderg29@gmail.com>
2024-09-03 11:04:48 -0500

    added name and major

--- hello.c
+++ hello.c
@@ -9,5 +9,7 @@ int main(int argc, char **argv){
     printf("Trung Huynh\n");
     printf("Major is undeclared\n");
 
+    printf("alexzander George\n");
+    printf("my majore is electrical engineering\n");
     return 0;
 }
